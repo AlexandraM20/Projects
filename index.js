071d65f8860a79fea0f5b83dc08181b4a8918510
AlexandraM20 <71652511+AlexandraM20@users.noreply.github.com>
2020-09-23 14:11:41 +0300

    Error fixed and I tried to display menu in console

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const {Builder} = require('selenium-webdriver');
+const { Builder } = require("selenium-webdriver");
+
 async function seleniumExample() {
-    const driver = await new Builder().forBrowser('firefox').build();
-    await driver.get('https://www.segracatering.ro/catering-zilnic.html');
-    let productName = document.querySelectorAll('.ta-left');
-    
+  const driver = await new Builder().forBrowser("firefox").build();
+  await driver.get("https://www.segracatering.ro/catering-zilnic.html");
+
+  let products = await driver.executeScript(
+    () => document.querySelectorAll("td.ta-left p")
+  );
+  let mapProducts = products.map(x => x.innerText);
+
+  setTimeout(function () {
+    console.log(mapProducts);
+  }, 4000);
 }
 
 seleniumExample();
